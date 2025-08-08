076b71d2d33aa174cb55d281db096a11ffc6cbb5
sajjad zeraatkar <73385188+sajjadzea@users.noreply.github.com>
2025-08-08 07:57:01 +0330

    fix: configure Vite for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,19 +3,21 @@ import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  base: "",
+  base: '/zero-day-of-water/',
+  root: 'src',
   plugins: [
     react(),
-    viteSingleFile()
+    viteSingleFile(),
   ],
   build: {
-    outDir: "docs",
+    outDir: '../docs',
+    emptyOutDir: true,
     assetsInlineLimit: 100000000,
     cssCodeSplit: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
-  }
+        manualChunks: undefined,
+      },
+    },
+  },
 });
